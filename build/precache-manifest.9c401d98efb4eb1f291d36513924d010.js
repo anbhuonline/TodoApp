5f05a6f4ab741c9eade8881f459066ce9388b064
anbhu <anbhuars@gmail.com>
2020-05-01 23:14:54 +0530

    first commit

---
+++ build/precache-manifest.9c401d98efb4eb1f291d36513924d010.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee39930544af37189f53f7363d72138a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73823ba0ad1a9d1a7433",
+    "url": "/static/css/2.1087be86.chunk.css"
+  },
+  {
+    "revision": "c1b158f83fe911254c69",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "73823ba0ad1a9d1a7433",
+    "url": "/static/js/2.6b27b2c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6b27b2c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1b158f83fe911254c69",
+    "url": "/static/js/main.91a0666f.chunk.js"
+  },
+  {
+    "revision": "faf04ed63a1a9030679e",
+    "url": "/static/js/runtime-main.fd2d341f.js"
+  }
+]);
